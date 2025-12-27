877a0c362cc9c9cb3501479fb2df56cf6f964dfc
omerpresler <41909419+omerpresler@users.noreply.github.com>
2025-12-27 13:53:10 +0200

    Add verbose logging control for bloom driver

--- src/paper/stp_bihs_bloom/Driver.h
+++ src/paper/stp_bihs_bloom/Driver.h
@@ -31,11 +31,11 @@ int solve_at_depth(MNPuzzleState<MN_SIZE, MN_SIZE> start,
                    MNPuzzleState<MN_SIZE, MN_SIZE> goal, int forwardDepth,
                    int backwardDepth, int size_in_KiB, int k_hashes,
                    int minItemsInserted, BloomType bloomType, double set_ratio,
-                   std::ostream *logFile = nullptr);
+                   std::ostream *logFile = nullptr, bool verbose = false);
 
 int benchmark(MNPuzzleState<MN_SIZE, MN_SIZE> start,
               MNPuzzleState<MN_SIZE, MN_SIZE> goal, int depth, int puzzle,
-              std::ofstream &logFile);
+              std::ofstream &logFile, bool verbose = false);
 
 std::vector<MNPuzzleState<MN_SIZE, MN_SIZE>>
 generateRandomState(int distance, int amount,
